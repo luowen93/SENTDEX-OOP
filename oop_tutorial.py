e4185f1cf69cde71c5462198eab3803f9199c535
luowen93 <owenlu93@gmail.com>
2021-02-25 16:50:20 -0800

    Finished video 14

--- oop_tutorial.py
+++ oop_tutorial.py
@@ -1,5 +1,18 @@
-class Blob:
+import pygame
+import random
 
+WIDTH = 800
+HEIGHT = 600
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+game_display = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Blob World")
+clock = pygame.time.Clock()
+
+class Blob:
     # Set initial variables
     def __init__(self, color):
         self.x = random.randrange(0, WIDTH) # Inside the window for hte game
@@ -18,3 +31,26 @@ class Blob:
 
         if self.y < 0: self.y = 0
         elif self.y > HEIGHT: self.y = HEIGHT
+
+def draw_environment(blob):
+    game_display.fill(WHITE) # Clear the background
+    pygame.draw.circle(game_display,blob.color,[blob.x, blob.y],blob.size)
+    pygame.display.update()
+    blob.move() # Update the position randomly
+
+def main():
+    red_blob = Blob(color=RED)
+    while True: # Infinite loop
+        for event in pygame.event.get(): # Grab event from the queue
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        draw_environment(red_blob)
+        clock.tick(60) # Capping frames per second
+        # update
+
+
+
+if __name__ == '__main__': ## __name__ is a special variable set to __main__ within this scope
+    main()
