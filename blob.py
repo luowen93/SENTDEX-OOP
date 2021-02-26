17737bb8a95eb4642b37db983f3fff19c507a55b
luowen93 <owenlu93@gmail.com>
2021-02-25 18:18:18 -0800

    Finished video 16

---
+++ blob.py
@@ -0,0 +1,29 @@
+## Blob class for import modularity testing
+import random
+
+class Blob:
+    # Set initial variables
+    def __init__(self, color, x_bnd, y_bnd, stepRange = [-1,2], sizeRange = [4,8]):
+        self.x_bnd = x_bnd
+        self.y_bnd = y_bnd
+        self.x = random.randrange(0, self.x_bnd) # Inside the window for hte game
+        self.y = random.randrange(0, self.y_bnd) # Inside the window
+        self.size = random.randrange(sizeRange[0],sizeRange[1]) # Size of the blob
+        self.color = color
+        self.x_bnd = x_bnd
+        self.y_bnd = y_bnd
+        self.stepRange = stepRange
+
+    def move(self):
+        self.move_x = random.randrange(self.stepRange[0],self.stepRange[1]) # Don't really understand what's happening here
+        self.move_y = random.randrange(self.stepRange[0],self.stepRange[1])
+        self.x += self.move_x
+        self.y += self.move_y
+
+    def check_bounds(self):
+        # Boundary conditions so blobs don't move out of bounds
+        if self.x < 0: self.x = 0
+        elif self.x > self.x_bnd: self.x = self.x_bnd
+
+        if self.y < 0: self.y = 0
+        elif self.y > self.y_bnd: self.y = self.y_bnd
--- oop_tutorial.py
+++ oop_tutorial.py
@@ -1,12 +1,12 @@
 import pygame
-import random
+from blob import Blob # Requires blob.py class to be in the same folder
 
 STARTING_BLUE_BLOBS = 10
 STARTING_RED_BLOBS = 3
 
-
 WIDTH = 800
 HEIGHT = 600
+STEP = [-4,4]
 
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
@@ -16,26 +16,6 @@ game_display = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Blob World")
 clock = pygame.time.Clock()
 
-class Blob:
-    # Set initial variables
-    def __init__(self, color):
-        self.x = random.randrange(0, WIDTH) # Inside the window for hte game
-        self.y = random.randrange(0, HEIGHT) # Inside the window
-        self.size = random.randrange(4,8) # Size of the blob
-        self.color = color
-
-    def move(self):
-        self.move_x = random.randrange(-1,2) # Don't really understand what's happening here
-        self.move_y = random.randrange(-1,2)
-        self.x += self.move_x
-        self.y += self.move_y
-
-        if self.x < 0: self.x = 0
-        elif self.x > WIDTH: self.x = WIDTH
-
-        if self.y < 0: self.y = 0
-        elif self.y > HEIGHT: self.y = HEIGHT
-
 def draw_environment(blob_list_dict):
     game_display.fill(WHITE) # Clear the background
 
@@ -43,13 +23,15 @@ def draw_environment(blob_list_dict):
         for blob_id in blob_dict:
             blob = blob_dict[blob_id] # Take the individual blob value using id
             pygame.draw.circle(game_display,blob.color,[blob.x, blob.y],blob.size)
-            pygame.display.update()
             blob.move() # Update the position randomly
+            blob.check_bounds()
+
+    pygame.display.update()
 
 def main():
-    blue_blobs = [Blob(BLUE) for i in range(STARTING_BLUE_BLOBS)] # List comprehension for creating list of blue blobs
+    blue_blobs = [Blob(BLUE,WIDTH,HEIGHT,STEP) for i in range(STARTING_BLUE_BLOBS)] # List comprehension for creating list of blue blobs
     blue_blobs = dict(enumerate((blue_blobs))) # Change into a dictionary with numbers being ID's starting at 0
-    red_blobs = [Blob(RED) for i in range(STARTING_RED_BLOBS)] # List comprehension for create list of red blobs
+    red_blobs = [Blob(RED,WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)] # List comprehension for create list of red blobs
     red_blobs = dict(enumerate(red_blobs))
     while True: # Infinite loop
         for event in pygame.event.get(): # Grab event from the queue
@@ -60,8 +42,6 @@ def main():
         draw_environment([blue_blobs,red_blobs])
         clock.tick(60) # Capping frames per second
         # update
-
-
 
 if __name__ == '__main__': ## __name__ is a special variable set to __main__ within this scope
     main()
