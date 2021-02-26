14fd6eea4fa1977422af94ff9d237c6e5fe3348f
luowen93 <owenlu93@gmail.com>
2021-02-25 17:03:36 -0800

    Finished video 15

--- oop_tutorial.py
+++ oop_tutorial.py
@@ -1,6 +1,10 @@
 import pygame
 import random
 
+STARTING_BLUE_BLOBS = 10
+STARTING_RED_BLOBS = 3
+
+
 WIDTH = 800
 HEIGHT = 600
 
@@ -32,21 +36,28 @@ class Blob:
         if self.y < 0: self.y = 0
         elif self.y > HEIGHT: self.y = HEIGHT
 
-def draw_environment(blob):
+def draw_environment(blob_list_dict):
     game_display.fill(WHITE) # Clear the background
-    pygame.draw.circle(game_display,blob.color,[blob.x, blob.y],blob.size)
-    pygame.display.update()
-    blob.move() # Update the position randomly
+
+    for blob_dict in blob_list_dict: # Loop through all dictionary
+        for blob_id in blob_dict:
+            blob = blob_dict[blob_id] # Take the individual blob value using id
+            pygame.draw.circle(game_display,blob.color,[blob.x, blob.y],blob.size)
+            pygame.display.update()
+            blob.move() # Update the position randomly
 
 def main():
-    red_blob = Blob(color=RED)
+    blue_blobs = [Blob(BLUE) for i in range(STARTING_BLUE_BLOBS)] # List comprehension for creating list of blue blobs
+    blue_blobs = dict(enumerate((blue_blobs))) # Change into a dictionary with numbers being ID's starting at 0
+    red_blobs = [Blob(RED) for i in range(STARTING_RED_BLOBS)] # List comprehension for create list of red blobs
+    red_blobs = dict(enumerate(red_blobs))
     while True: # Infinite loop
         for event in pygame.event.get(): # Grab event from the queue
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        draw_environment(red_blob)
+        draw_environment([blue_blobs,red_blobs])
         clock.tick(60) # Capping frames per second
         # update
 
